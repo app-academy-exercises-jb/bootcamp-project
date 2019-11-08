eb2d7ad00ac9f797d66a372e1910fd8458ad6efe
Jorge Barreto <jrbd93@gmail.com>
2019-11-08 13:45:44 -0500

    final

---
+++ .history/lib/bootcamp_20191108132108.rb
@@ -0,0 +1,42 @@
+class Bootcamp
+    def initialize(name, slogan, student_capacity)
+        @name, @slogan, @student_capacity = name, slogan, student_capacity
+        @teachers, @students, @grades = Array.new, Array.new, Hash.new { |h, k| h[k] = Array.new }
+    end
+
+    def name
+        @name
+    end
+
+    def slogan
+        @slogan
+    end
+
+    def teachers
+        @teachers
+    end
+
+    def students
+        @students
+    end
+
+    def hire=(prof)
+        @teachers << prof
+    end
+
+    def enroll=(student)
+        if @students.length < @student_capacity
+            @students << student
+            return true
+        else
+            return false
+        end
+    end
+
+    def enrolled?=(student)
+        @students.include?(student)
+    end
+
+    
+
+end
